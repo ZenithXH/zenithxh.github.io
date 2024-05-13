c6134e9ae199df598904694166d73a3a5e8e7985
ZenithXH <qq3070655970@163.com>
2024-05-14 00:49:39 +0800

    Site updated: 2024-05-14 00:49:39

--- sw.js
+++ sw.js
@@ -21,7 +21,7 @@ self.addEventListener('active', function () {
 });
 
 var precacheUrls = [
-'/','/posts/31708/','/posts/43590/','/posts/26104/','/posts/45029/','/about/index.html','/extension/content.json','/gallery/index.html','/letter/index.html','/icons/index.html','/library/index.html','/records/index.html','/records/content.json','/resume/index.html','/extension/letter/index.js','/extension/letter/index.css','/extension/resume/index.js','/extension/resume/index.css','/extension/letter/chinese/data.json','/extension/letter/english/data.json','/extension/resume/chinese/data.json','/extension/resume/english/data.json',
+'/','/posts/31708/','/posts/43590/','/posts/26104/','/posts/45029/','/about/index.html','/extension/content.json','/gallery/index.html','/icons/index.html','/letter/index.html','/library/index.html','/records/content.json','/records/index.html','/resume/index.html','/extension/letter/index.js','/extension/letter/index.css','/extension/resume/index.js','/extension/letter/english/data.json','/extension/letter/chinese/data.json','/extension/resume/chinese/data.json','/extension/resume/index.css','/extension/resume/english/data.json',
 ];
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
